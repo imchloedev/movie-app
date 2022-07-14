3b7d16fa1fe461f8fab4370a2c45158666fedd76
Eunji Kang <etothej@naver.com>
2022-07-14 20:16:17 +0900

    components and style added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Footer from '../src/components/Footer'
+import Header from '../src/components/Header'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Header/>
+    <Component {...pageProps} />
+    <Footer />
+    </>
+    
+  )
 }
 
 export default MyApp
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <Link href="/">
+        <a>
+          <div>
+            <img
+              className={styles.logo_img}
+              src="images/logo.png"
+              alt="logo"
+              style={{
+                width: 90,
+                height: 80,
+              }}
+            />
+          </div>
+        </a>
+      </Link>
+
+      <div className={styles.gnb}>
+        <ul>
+          <li>Home</li>
+          <li>Movies</li>
+          <li>TV Series</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
